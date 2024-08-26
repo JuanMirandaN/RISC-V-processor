11d91a37663565d157d405b6795c25530074587c
Ign1s <132116182+Ign1s-08@users.noreply.github.com>
2024-08-26 08:17:09 -0400

    Add the multicycle vivado project

---
+++ Risc_V_Multicycle/Risc_V_Multicycle.srcs/sources_1/new/RISC_V_top.sv
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 19.08.2024 08:59:27
+// Design Name: 
+// Module Name: RISC_V_top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RISC_V_top(
+
+    );
+endmodule
+
+module register(output logic [31:0] Q, input logic [31:0] D,clk);
+
+endmodule:register
+
+
