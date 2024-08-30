85bd84cc15563b14a90d11aca1dfd1ccc8dad0ce
Ign1s <132116182+Ign1s-08@users.noreply.github.com>
2024-08-30 10:52:57 -0400

    add the register file block

--- Risc_V_Multicycle/Risc_V_Multicycle.srcs/sources_1/new/RISC_V_top.sv
+++ Risc_V_Multicycle/Risc_V_Multicycle.srcs/sources_1/new/RISC_V_top.sv
@@ -25,16 +25,35 @@ module RISC_V_top(
     );
 endmodule
 
-module register(output logic [31:0] Q, input logic [31:0] D,clk,rst);
+module register(output logic [31:0] Q, input logic [31:0] D,input logic clk,rst,en);
 
     always_ff @(posedge clk or posedge rst) begin
-       if(rst)
-         Q <=32'b0; 
-       else
-         Q <= D;
+        if(rst)
+            Q <=32'b0; 
+        else if(en)
+            Q <= D;
     end 
     
 endmodule:register
+
+
+module register_file(input logic [4:0] A1,A2,A3,input logic [31:0] WD3, input logic clk,WE3, output logic [31:0] RD1,RD2);
+
+    logic [31:0] regs [31:0]; //defines 32 x 32-bit registers
+
+    assign regs[5'h00] = 32'h0000; //x0 register hardwired to 0
+    assign RD1 = regs[A1];
+    assign RD2 = regs[A2];
+
+    always_ff @(posedge clk) begin
+
+        if(WE3)
+            regs[A3] <= WD3;
+    end
+    
+endmodule:register_file
+
+
 
 
 
